41e376fc3d98b2be848a0e75c0916a2919e611c3
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com>
2024-10-19 22:25:48 +0500

    part 4 exe 10 done

---
+++ exe-10/tests/blog_api.test.js
@@ -0,0 +1,86 @@
+const { test, before, after } = require("node:test");
+const assert = require("node:assert");
+const supertest = require("supertest");
+const app = require("../app");
+const Blog = require("../models/blog");
+const mongoose = require("mongoose");
+
+const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: "First Blog",
+    author: "John Doe",
+    url: "http://example.com/first",
+    likes: 10,
+  },
+  {
+    title: "Second Blog",
+    author: "Jane Doe",
+    url: "http://example.com/second",
+    likes: 5,
+  },
+];
+
+before(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(initialBlogs);
+});
+
+test("GET /api/blogs - blogs are returned as JSON", async () => {
+  const response = await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("GET /api/blogs - the correct number of blogs is returned", async () => {
+  const response = await api.get("/api/blogs");
+  assert.strictEqual(response.body.length, initialBlogs.length);
+});
+
+test("GET /api/blogs - the unique identifier property is named id", async () => {
+  const response = await api.get("/api/blogs");
+
+  response.body.forEach((blog) => {
+    assert.strictEqual(blog.id !== undefined, true);
+    assert.strictEqual(blog._id === undefined, true);
+  });
+});
+
+// Test that a new blog can be created
+test("POST /api/blogs - a new blog can be added", async () => {
+  const newBlog = {
+    title: "New Blog",
+    author: "New Author",
+    url: "http://example.com/new",
+    likes: 15,
+  };
+
+  // Fetch initial blogs
+  const initialResponse = await api.get("/api/blogs");
+  const initialBlogsCount = initialResponse.body.length;
+
+  // Send a POST request to add a new blog
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  // Fetch the blogs again to confirm the new blog is added
+  const updatedResponse = await api.get("/api/blogs");
+  const updatedBlogs = updatedResponse.body;
+
+  // Verify the total number of blogs increased by 1
+  assert.strictEqual(updatedBlogs.length, initialBlogsCount + 1);
+
+  // Verify that the new blog is saved with the correct content
+  const titles = updatedBlogs.map((blog) => blog.title);
+  assert.strictEqual(titles.includes("New Blog"), true);
+});
+
+// Cleanup after tests
+after(async () => {
+  await mongoose.connection.close();
+});
